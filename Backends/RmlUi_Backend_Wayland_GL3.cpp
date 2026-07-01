d1376b6b86a01c5d4d6c95b00c53099816338697
Third-Thing <219055174+Third-Thing@users.noreply.github.com>
2026-07-01 15:43:06 -0700

    Restore Wayland swap interval

--- Backends/RmlUi_Backend_Wayland_GL3.cpp
+++ Backends/RmlUi_Backend_Wayland_GL3.cpp
@@ -534,7 +534,7 @@ static bool InitializeEGL()
 	if (!eglMakeCurrent(data->egl_display, data->egl_surface, data->egl_surface, data->egl_context))
 		return false;
 
-	eglSwapInterval(data->egl_display, 0);
+	eglSwapInterval(data->egl_display, 1);
 	return true;
 }
 
